70ec368143bee3d6a774b857fe062dc941bb0382
yerinto martinez <yerinto.martinez@hotmail.fr>
2022-11-15 18:19:39 +0100

    rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_095547) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_15_125527) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
-    t.boolean "false"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
